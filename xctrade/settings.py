3cfa73b54d9a903d16150a84b1e72d1ba89cf7ea
Jamie Parkinson <jamie.parkinson@gmail.com>
2021-04-03 17:18:13 +0100

    Update settings.py

--- xctrade/settings.py
+++ xctrade/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,8 +92,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+ci = os.environ["CI"] if "CI" in os.environ else False
+local = socket.gethostname() == "atmlxlap005"
+if not (ci or local):
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation

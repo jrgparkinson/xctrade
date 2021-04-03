bc2723808cf8220c06b9466d861882e22d47d6fe
Jamie Parkinson <jamie.parkinson@gmail.com>
2021-04-03 13:42:49 +0100

    Create BottomNav.js

---
+++ src/components/BottomNav.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+export default function Header() {
+  return (
+
+<BottomNavigation value={value} onChange={this.handleChange}>
+    <BottomNavigationAction
+        component={Link}
+        to="/Athletes"
+        label="Athletes"
+        value="Athletes"
+    />
+<BottomNavigationAction
+        component={Link}
+        to="/Orders"
+        label="Orders"
+        value="Orders"
+        
+    />
+</BottomNavigation
+);
+}
